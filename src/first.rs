d0f60af4c34566d0c81e25ae88477cb9bb6047cf
Ray Kochenderfer <35624960+GimpFlamingo@users.noreply.github.com>
2020-02-08 21:23:45 -0500

    Added push and pop

--- src/first.rs
+++ src/first.rs
@@ -1,11 +1,7 @@
+use std::mem;
+
 pub struct List {
 	head: Link,
-}
-
-impl List {
-	pub fn new() -> Self {
-		List { head: Link::Empty }
-	}
 }
 
 enum Link {
@@ -15,5 +11,32 @@ enum Link {
 
 struct Node {
 	elem: i32,
-	next: List,
+	next: Link,
+}
+
+impl List {
+	/// # Pushes a node onto the list
+	pub fn push(&mut self, elem: i32) {
+		let new_node = Box::new(Node {
+			elem: elem,
+			next: mem::replace(&mut self.head, Link::Empty),
+		});
+
+		self.head = Link::More(new_node);
+	}
+
+	/// # Removes a node from the front of a list
+	pub fn pop(&mut self) -> Option<i32> {
+		match mem::replace(&mut self.head, Link::Empty) {
+			Link::Empty	=> None,
+			Link::More(node) => {
+				self.head = node.next;
+				Some(node.elem)
+			}
+		}
+	}
+
+	pub fn new() -> Self {
+		List { head: Link::Empty }
+	}
 }
